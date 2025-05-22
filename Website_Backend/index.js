abedc5cbce0ff4c90b83e2930e731046dd677ebc
Sampann Neema <sampannneema9@gmail.com>
2025-05-21 23:08:59 -0700

    Done project

--- Website_Backend/index.js
+++ Website_Backend/index.js
@@ -30,7 +30,7 @@ app.use(userRouter);
 app.use(hostRouter);
 
 // MongoDB Connection + Server Start
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
